002b9e8ad989bcc69c3d9937e654658a3bf73cc2
Antonio Abraham <tony.calablava@gmail.com>
2019-10-22 15:23:48 -0400

    Fixed customer_controller line 31 unneccesary params, reviews controller done

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,3 +1,52 @@
 class ReviewsController < ApplicationController
+  # def index
+  #   @reviews = Review.all
+  # end
+
+  def show
+    @review = Review.find(params[:id])
+  end
+
+
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    if @review.valid?
+      @review.save
+      redirect_to review_path(@review)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @review = Review.find(params[:id])
+  end
+
+  def update
+    @review = Review.find(params[:id])
+    @review.update(review_params)
+    if @review.update
+     redirect_to review_path(@review)
+    else
+      render :edit
+   end
+  end
+
+
+  def destroy
+    @review = Review.find(params[:id])
+  @review.destroy
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :content, :customer_id, :item_id)
+  end
 
 end
