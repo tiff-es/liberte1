68ab516f3e357b319c9a5fed773b4638c5cc9bc4
Antonio Abraham <tony.calablava@gmail.com>
2019-10-22 12:02:47 -0400

    Customers merged, customer all  resources routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :items, only: [:index, :show]
   resources :categories, only: [:index, :show]
+  resources :customers
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
