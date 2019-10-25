f131eb7b790001f1dbd9f508ec4ea0d63c6bd1f8
github username <github email address>
2019-10-25 03:49:28 -0400

    login page still needs work and still need to work on lots of other stuff

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,17 @@
+
 class SessionsController < ApplicationController
+
   def login
+  end
+
+  def process_login
+    user = Customer.find_by(username: params[:username])
+    if user
+      session[:user_id] = user.id
+      redirect_to categories_path
+    else
+      flash.now["notice"] = "No user found with this username."
+      render :login
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'sessions/login'
+  # get 'sessions/login'
   resources :items, only: [:index, :show, :update, :destroy]
   resources :categories, only: [:index, :show]
   resources :customers
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   resources :purchases, only: [:index, :show]
 
   get "/login", to: 'sessions#login'
+  post "/login", to: "sessions#process_login"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
